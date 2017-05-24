2801638c98777376d2b3110b22d0298fcc950d55
Phillip Gray <philgrayphilgray@Phillips-iMac.local>
2017-05-23 23:09:53 -0400

    setup assignment, create components, setup slots, setup dynamic loading with component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template lang="pug">
 .container
-  h1 Initial Setup
+  .row
+    .col-sm-12
+      br
+      button.btn.btn-primary(@click='selectedComponent="appBlue"') Load Blue template
+      button.btn.btn-success(@click='selectedComponent="appGreen"') Load Green template
+      button.btn.btn-danger(@click='selectedComponent="appRed"') Load Red Template
+      hr
+      component(:is='selectedComponent')
+        h2 {{ selectedComponent }}
 </template>
 
 <script>
+import Blue from './components/Blue.vue';
+import Green from './components/Green.vue';
+import Red from './components/Red.vue';
 export default {
+  data: function(){
+      return{
+            selectedComponent: ''
+      }
+    },
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  }
 }
 </script>
 
---
+++ src/components/Red.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+div
+  slot
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+div{
+  background-color: red;
+  padding: 30px;
+  margin: 20px auto;
+  text-align: center;
+  color: #fff;
+}
+</style>
