943e64d4224eb9b727c51c41c0191d682a210f14
Phillip Gray <philgrayphilgray@Phillips-iMac.local>
2017-05-27 17:37:52 -0400

    complete lectures 156 through 163

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template lang="pug">
 .container
-  h1 Initial Setup
+  h1 Custom Directives
+  .card
+    .card-block
+      .card-title
+        h2 Highlight directive with bind hook
+      .card-text
+        p(v-highlight.delayed='"red"') test
 </template>
 
 <script>
 export default {
+  data: function(){
+    return {
+
+    }
+  }
 }
 </script>
 
