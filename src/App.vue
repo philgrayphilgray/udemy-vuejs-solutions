d3991979b565e42fd84e758db3e55addb91dca07
Phillip Gray <philgrayphilgray@Phillips-iMac.local>
2017-05-28 10:16:16 -0400

    complete filters lectures

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template lang="pug">
 .container
-  h1 Initial Setup
+  .row
+    .col-sm-12
+      h1 Filters & Mixins
+      p {{ text | toUppercase | to-lowercase }}
+      input(v-model='filterText')
+      ul
+        li(v-for='fruit in searchFruits') {{ fruit }}
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      text: 'Hello!',
+      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+      filterText: ''
+    }
+  },
+  filters: {
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  },
+  computed: {
+    searchFruits(){
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      });
+    }
+  }
 }
 </script>
 
