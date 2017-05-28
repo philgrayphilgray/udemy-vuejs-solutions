cc8356c2930570387c126bd188181a12f6c95628
Phillip Gray <philgrayphilgray@Phillips-iMac.local>
2017-05-27 21:43:46 -0400

    complete section 12

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       .card-title
         h2 Highlight directive with bind hook
       .card-text
-        p(v-highlight.delayed='"red"') test
+        p(v-local-highlight.delayed.blink='{mainColor: "red", secondColor: "green", delay: 500}') test
 </template>
 
 <script>
@@ -14,6 +14,47 @@ export default {
   data: function(){
     return {
 
+    }
+  },
+  directives: {
+    'local-highlight': {
+      bind(el, binding, vnode){
+        // el.style.backgroundColor = 'green';
+        // el.style.backgroundColor = binding.value;
+        let delay = 0;
+        if(binding.modifiers['delayed']){
+          delay = 3000;
+        }
+        if(binding.modifiers['blink']){
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+              if(binding.arg == 'background'){
+                el.style.backgroundColor = currentColor;
+              }
+              else {
+                el.style.color = currentColor;
+              }
+            }, binding.value.delay);
+
+          }, delay);
+        }
+        else{
+          setTimeout(() => {
+            if(binding.arg == 'background'){
+              el.style.backgroundColor = binding.value.mainColor;
+            }
+            else {
+              el.style.color = binding.value.mainColor;
+            }
+          }, delay);
+        }
+
+
+      }
     }
   }
 }
