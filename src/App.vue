e0f249b8114b7b6900e5b90308b1c51798e4dad0
Phillip Gray <philgrayphilgray@Phillips-iMac.local>
2017-05-29 14:04:34 -0400

    setup vueResource and firebase conncection http options root

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,60 @@
 <template lang="pug">
 .container
-  h1 Initial Setup
+  .row
+    .col-sm-12
+      h1 Http
+      div.form-group
+        label Username
+        input.form-control(type='text', v-model='user.username')
+      div.form-group
+        label Email
+        input.form-control(type='email', v-model='user.email')
+      button.btn.btn-primary(@click='submit') Submit
+      hr
+      button.btn.btn-primary(@click='fetchData') Get Data
+      br
+      br
+      ul.list-group
+        li.list-group-item(v-for='u in users') {{ u.username}} - {{ u.email }}
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      user: {
+        username: '',
+        email: ''
+      },
+      users: []
+    }
+  },
+  methods: {
+    submit(){
+      console.log(this.user);
+      this.$http.post('', this.user)
+        .then(response => {
+          console.log(response);
+        }, error => {
+          console.log(error);
+        });
+    },
+    fetchData(){
+      this.$http.get('')
+      .then(response => {
+        return response.json();
+      }, error => {
+        console.log(error);
+      })
+      .then(data => {
+        const resultArray= [];
+        for( let key in data){
+          resultArray.push(data[key]);
+        }
+        this.users = resultArray;
+      });
+  }
+}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-http-10290.firebaseio.com/data.json';
 
 new Vue({
   el: '#app',
